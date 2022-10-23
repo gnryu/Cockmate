49661e01470179fc98042b8b891ceb1c2da04137
Gina Ryu <ginaryu@Ginaui-MacBookPro.local>
2022-10-24 05:10:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cockmate/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.example.cockmate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button mLoginBtn;
+    TextView mJoinText;
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        mJoinText = findViewById(R.id.join_text);
+        mLoginBtn = findViewById(R.id.login_btn);
+        mEmailText = findViewById(R.id.m_user_email);
+        mPasswordText = findViewById(R.id.m_user_password);
+
+        // 가입 버튼이 눌리면
+        mJoinText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, JoinActivity.class));
+            }
+        });
+
+        // 로그인 버튼이 눌리면
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+                firebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
